6452e96fbfc901c61261b4cf97e414ce7f74f369
Deepak10220 <deepakydv1026@.com>
2024-05-03 01:54:03 +0530

    update file

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const db = require("./db.js");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-require('dotenv').config();
-
-const PORT = process.env.PORT || 3000;
 
 const personRouter = require("./routes/personRoute.js");
 const menuRouter = require("./routes/menuRoute.js");
